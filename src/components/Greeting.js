4b0ff38f689deba08fcaee0c375a313d31d92883
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-29 23:22:51 +0500

    Add linters setup and remove linters error

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getMessage } from "../redux/greetingReducer";
 import axios from "axios";
+import { getMessage } from "../redux/greetingReducer";
 
 const Greeting = () => {
   const dispatch = useDispatch();
@@ -20,7 +20,9 @@ const Greeting = () => {
   return (
     <div>
       <h1>{message}</h1>
-      <button onClick={() => fetchMessage()}>Generate greeting message</button>
+      <button type="button" onClick={() => fetchMessage()}>
+        Generate greeting message
+      </button>
     </div>
   );
 };
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import greetingsReducer from "./greetingReducer";
+import { configureStore } from '@reduxjs/toolkit';
+import greetingsReducer from './greetingReducer';
 
 const store = configureStore({
   reducer: {
