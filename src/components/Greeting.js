60b3116850d98118cd21eb135a9fdce86561b462
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-29 23:26:22 +0500

    Remove linters error

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { getMessage } from "../redux/greetingReducer";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { getMessage } from '../redux/greetingReducer';
 
 const Greeting = () => {
   const dispatch = useDispatch();
   const message = useSelector((state) => state.greeting);
 
   async function fetchMessage() {
-    await axios.get("http://127.0.0.1:3000/api/messages").then((response) => {
+    await axios.get('http://127.0.0.1:3000/api/messages').then((response) => {
       dispatch(getMessage(response.data.greeting));
     });
   }
